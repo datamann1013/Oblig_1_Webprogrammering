8a41c7fbce6363534b66929d1eb152e91da45d45
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2024-02-04 10:47:18 +0100

    -third commit, update index.html, js.js, README.md

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-Oblig 2
+Oblig 1
 =======
-OsloMet brukernavn: 
+OsloMet brukernavn: marom4286
 
-GitHub brukernavn: 
+GitHub brukernavn: datamann1013
 
-Github repo URL: 
+Github repo URL: https://github.com/data1700-v23/oblig-1-datamann1013/tree/master
 
-Fullt navn: 
+Fullt navn: Martin Storødegård Rømmesmo
 
-Kort beskrivelse av applikasjon (5-10 setninger):
+Kort beskrivelse av applikasjon (5-10 setninger): Et billettbestillingssystem med validering av verdier i feltene. 
+Applikasjonen gir alert/popup boxes ved feil input. Informasjonen legges så i et array og printes på nettsiden ved hjelp av forms.
+Det er også en knapp som nullstiller arrayet slik at billettene slettes og infoen på siden slettes og.
---
+++ src/main/resources/static/js.js
@@ -0,0 +1,49 @@
+let ticketArray = [];
+
+function ticket(){
+    let inputBoxMovie = document.getElementById("filmselector").value;
+    let inputBoxAmount = document.getElementById("amount").value;
+    let inputBoxFirst = document.getElementById("firstName").value;
+    let inputBoxSur = document.getElementById("surName").value;
+    let inputBoxPhone = document.getElementById("telephoneNR").value;
+    let inputBoxEmail = document.getElementById("email").value;
+    if (inputBoxMovie == 'Velg film her'){
+        alert('Velg en film!')
+        return;
+    }
+    let fullInput = {
+        // Using : instead of = to make more compact / use less characters
+        "filmselector" : inputBoxMovie,
+        "amount" : inputBoxAmount,
+        "firstName" : inputBoxFirst,
+        "surName" : inputBoxSur,
+        "telephoneNR" : inputBoxPhone,
+        "email" :inputBoxEmail
+    }
+    ticketArray.push(fullInput);
+    console.log(fullInput);
+    document.getElementById('filmselector').value = 'Velg film her';
+    document.getElementById('amount').value = ' ';
+    document.getElementById('firstName').value = ' ';
+    document.getElementById('surName').value = ' ';
+    document.getElementById('telephoneNR').value = ' ';
+    document.getElementById('email').value = ' ';
+    out1();
+}
+
+function out1(){
+    let ut = "<table><tr><td>Fornavn</td><td>Etternavn</td><td>Telefonnr</td>" +
+        "<td>Epost</td><td>Film</td><td>Antall billetter</td></tr>";
+
+    for (let a = 0; a < ticketArray.length; a++){
+        ut = ut + "<tr><td>" + ticketArray[a].firstName + "</td><td>" + ticketArray[a].surName + "</td><td>" +
+            ticketArray[a].telephoneNR + "</td><td>" + ticketArray[a].email + "</td><td>" +
+            ticketArray[a].filmselector + "</td><td>" + ticketArray[a].amount + "</td></tr>";
+    }
+    document.getElementById('output').innerHTML = ut;
+    console.log(ut);
+}
+function delTickets(){
+    ticketArray = [];
+    out1();
+}
